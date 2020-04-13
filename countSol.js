27043b7166adde52afa8269806f614a8772ffa5d
Peter Knipp <peteraknipp@gmail.com>
2020-04-13 19:19:49 -0400

    Create countSol.js, which simply counts number of solutions

---
+++ countSol.js
@@ -0,0 +1,57 @@
+const totalNQueens = n => {
+
+  const QUEEN = "Q";
+  const BLANK = " ";
+  const ATTACKED = ".";
+  let boardSolutions = 0;
+
+  const copyBoard = board => {
+    const boardCopy = [];
+    for (const row of board) {
+      boardCopy.push(row.slice());
+    }
+    return boardCopy;
+  }
+
+  const placeQueen = (board, row, col) => {
+
+    board[row][col] = QUEEN;
+
+    for (let r = 0; r < n; r++) {
+      for (let c = 0; c < n; c ++) {
+        if (board[r][c] === BLANK) {
+          if (Math.abs(c - col) === Math.abs(r - row) || c === col || r === row) {
+            board[r][c] = ATTACKED;
+          }
+        }
+      }
+    }
+  }
+
+  const processRow = (board, row = 0) => {
+
+    if (row === n) {
+      boardSolutions++;
+      return;
+    }
+
+    let col = 0;
+    let nextSpace;
+    while ((nextSpace = board[row].indexOf(BLANK, col)) > -1) {
+      const thisBoard = copyBoard(board);
+      placeQueen(thisBoard, row, nextSpace);
+      processRow(thisBoard, row + 1);
+      col = nextSpace + 1;
+    }
+
+  }
+
+  const blankRow = Array(n).fill(BLANK);
+  const blankBoard = [...Array(n)].map(row => blankRow.slice());
+  processRow(blankBoard);
+
+  return boardSolutions;
+
+};
+
+console.log(totalNQueens(8));
