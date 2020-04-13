5e53cab94042a801693cd24a6772012835edc7b7
Peter Knipp <peteraknipp@gmail.com>
2020-04-13 15:39:19 -0400

    Consolidate some logic, remove some comments, and add several others

--- iterMult.js
+++ iterMult.js
@@ -5,7 +5,6 @@
 const solveNQueens = function(n) {
   // n equals the size of the board AND the number of queens
   // This is O(N!)
-  // If n was fixed we could use n nested loops, but alas that is not the case.
   const QUEEN = "Q";
   const BLANK = " ";
   const ATTACKED = ".";
@@ -14,32 +13,25 @@ const solveNQueens = function(n) {
 
   let board = Array(n).fill(Array(n).fill(BLANK));
 
-  // initialize n-element array positions = [-1, -1, -1, ..., -1]
-  // solution = [2,4,6,8,3,1,7,5].map(e => String(e--)).join('')
-  // change positions[0] to equal 0
-  // change positions[1] to equal the smallest value so that it does not attack other piece(s)
-  // change positions[2] to equal the  "             "
-  // ...
-  // change positions[n-1] to equal the "
+  // alternate form: solution = [2,4,6,8,3,1,7,5].map(e => String(e--)).join('')
 
   let placeNextQueen = function(board, row, col) {
 
     board[row][col] = QUEEN;
 
-    for (let r = 0; r < n; r++) {
-      if (board[r][col] === BLANK) {
-        board[r][col] = ATTACKED;
-      }
-    }
-    for (let c = 0; c < n; c++) {
-      if (board[row][c] === BLANK) {
-        board[row][c] = ATTACKED;
-      }
-    }
+    //PK thinks that a base case needs to be placed here, as follows:
+//    if (row === n) {return board; }
+
+/* PK consolidated these cases by expanding the if-condition below.
+    for (let r = 0; r < n; r++) {if (board[r][col] === BLANK) {board[r][col] = ATTACKED;}}
+    for (let c = 0; c < n; c++) {if (board[row][c] === BLANK) {board[row][c] = ATTACKED; }}
+*/
     for (let r = 0; r < n; r++) {
       for (let c = 0; c < n; c ++) {
         if (board[r][c] === BLANK) {
-          if (c + r === row + col || c - r === row - col) {
+          // if (c + r === col + row || c - r === col - row) {
+            // c - col === row - r || c - col === r - row
+          if (Math.abs(c - col) === Math.abs(r - row) || c === col || r === row) {
             board[r][c] = ATTACKED;
           }
         }
@@ -47,25 +39,26 @@ const solveNQueens = function(n) {
     }
     console.log(board);
 
-    // first call of this should invoke an empty board, but not subsequent calls
-    // Perhaps do with something like  board = board || empty board
-    // loop over columns, place a queen in that column and (which?) row, then assign attacking x's
-    // subsequently make n recursive calls
   }
 
   row = 0;
   col = 0;
   let nextSpace;
+  // I think that we've set the 2nd argument of .indexOf incorrectly.
+  // Rather than increasing col by one, we should increase it to equal
+  // the value of nextSpace (perhaps plus one)
   while ((nextSpace = board[row].indexOf(BLANK, col++)) > -1) {
     placeNextQueen(board, row, col)
   }
+  /* PK: Do we need to return something (null?) if there are no spaces available to allow
+  this while-loop to execute?  Also, don't we need to do something w/the return value of
+  these function calls?
+  */
 };
 
+/*PK: We have a chicken-and-egg problem w/regards to the first call of placeNextQueen:
+In which column do we put the first queen?  Do we do a loop and make n such calls, one
+for each column in which the queen is placed?
+*/
+
 solveNQueens(4);
-
-
-// place first Q @ a1
-// mark attacked squares
-// find 'next' open (non-Q, non-. square)
-//
-//
